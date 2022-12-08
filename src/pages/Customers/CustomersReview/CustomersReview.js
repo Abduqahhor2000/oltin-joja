63ba297d43ec7ceac40d9adfe72319d2c5040ee4
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-12-08 16:51:29 +0500

    add CustomsReview page

--- src/pages/Customers/CustomersReview/CustomersReview.js
+++ src/pages/Customers/CustomersReview/CustomersReview.js
@@ -1,16 +1,77 @@
 import React, { useState } from "react";
+import {half_star_svg, full_star_svg, empty_star_svg} from "../../../svg/customer"
+import customer_img from "../../../images/customer.png";
+import customer1_img from "../../../images/customer1.png";
+import customer2_img from "../../../images/customer2.png";
+import customer3_img from "../../../images/customer3.png";
+import customer5_img from "../../../images/customer5.png";
+import customer8_img from "../../../images/customer8.png";
 
 function CustomersReview() {
   const [comments, setComments] = useState([
     {
       id: 0,
+      image: customer_img,
+      fullName: "Aminjon Usmonov",
+      foodName: "Big Burgers",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 9,
+    },
+    {
+      id: 1,
+      image: customer1_img,
+      fullName: "Hakim Bakirov",
+      foodName: "Clab-Sandviches",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 9,
+    },
+    {
+      id: 2,
       image: null,
+      fullName: "Karimjon Usmonov",
+      foodName: "Big Burgers",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 6,
+    },
+    {
+      id: 3,
+      image: customer3_img,
+      fullName: "Elyor Olimov",
+      foodName: "Choykhona Osh",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 9,
+    },
+    {
+      id: 4,
+      image: customer8_img,
+      fullName: "Mahliyo Ilhamova",
+      foodName: "Fries",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 7,
+    },
+    {
+      id: 5,
+      image: customer2_img,
+      fullName: "Guli Yoldasheva",
+      foodName: "Big Burgers",
+      date: "June 28, 2019",
+      text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
+      ball: 9,
+    },
+    {
+      id: 6,
+      image: customer5_img,
       fullName: "Aminjon Usmonov",
       foodName: "Big Burgers",
       date: "June 28, 2019",
       text: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda.",
       ball: 8,
-    },
+    }
   ]);
   return (
     <div>
@@ -22,7 +83,7 @@ function CustomersReview() {
           <div className="max-h-[calc(100vh-180px)] overflow-y-auto py-5 px-7">
             {comments.map((comment) => {
               return (
-                <div className="flex">
+                <div key={comment.id} className="flex mb-7">
                   <div className="w-20 flex-none flex justify-center">
                     {comment.image ? (
                       <img
@@ -38,13 +99,23 @@ function CustomersReview() {
                       </>
                     )}
                   </div>
-                  <div className="grow bg-gray-200">
+                  <div className="grow">
                     <div className="flex justify-between">
                       <span className="font-medium">
                         <span className="mr-12 text-lg text-black">{comment.fullName}</span>
                         <span className="text-base text-Primary/03">{comment.foodName}</span>
                       </span>
                       <span className="text-black/40 text-base">{comment.date}</span>
+                    </div>
+                    <div className="flex">
+                      {
+                        [1, 3, 5, 7, 9].map((number)=> {
+                          return <span key={number} className="mx-1 mt-2.5 mb-3">{comment.ball > number ? full_star_svg : comment.ball === number ? half_star_svg : empty_star_svg}</span>
+                        })
+                      }
+                    </div>
+                    <div className="text-[15px] text-[#6F767E] pr-36">
+                      {comment.text}
                     </div>
                   </div>
                 </div>

64f3fcbddaf048f2bb24b9be2b2d74b672e81896
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-12-12 13:22:04 +0500

    fix addadmin page

--- src/pages/Settings/Admin/AddAdmin.js
+++ src/pages/Settings/Admin/AddAdmin.js
@@ -4,10 +4,10 @@ import Input from "./Components/Input";
 import ReactSelect from "./Components/Select";
 
 let position = [
-  {value: "Admin", label: "Admin"},
-  {value: "Sotuvchi", label: "Sotuvchi"},
-  {value: "Oshpaz", label: "Oshpaz"},
-]
+  { value: "Admin", label: "Admin" },
+  { value: "Sotuvchi", label: "Sotuvchi" },
+  { value: "Oshpaz", label: "Oshpaz" },
+];
 
 function AddAdmin() {
   const [touched, setTouched] = useState({});
@@ -34,148 +34,168 @@ function AddAdmin() {
           <span> / Add Admin</span>
         </div>
         <div className="min-h-[430px] bg-white rounded-[10px] p-7 pb-5">
-          <div className="flex mb-7">
-            <div className="grow pr-7 flex justify-between flex-wrap">
-              <div className="w-full">
-                <Input
-                  handleChange={handleChange}
-                  touched={touched}
-                  values={values}
-                  errors={errors}
-                  type={"full_name"}
-                  placeholderText={"Full name"}
-                />
-              </div>
-              <div className="w-[calc((100%-20px)/2)] text-Neutral/Shades/04-75% text-[15px]">
-                <ReactSelect
-                  handleChange={handleChange}
-                  touched={touched}
-                  values={values}
-                  errors={errors}
-                  type={"role"}
-                  position={position}
-                />
-              </div>
-              <div className="w-[calc((100%-20px)/2)]">
-                <Input
-                  handleChange={handleChange}
-                  touched={touched}
-                  values={values}
-                  errors={errors}
-                  type={"email"}   
-                  placeholderText={"Email"}
-                />
-              </div>
-              <div className="w-[calc((100%-20px)/2)]">
-                <Input
-                  handleChange={handleChange}
-                  touched={touched}
-                  values={values}
-                  errors={errors}
-                  type={"phone"}
-                  placeholderText={"Phone number"}
-                />
-              </div>
-              <div className="w-[calc((100%-20px)/2)]">
-                <Input
-                  handleChange={handleChange}
-                  touched={touched}
-                  values={values}
-                  errors={errors}
-                  type={"password"}
-                  placeholderText={"Password"}
-                />
-              </div>
-              <div className="w-full border border-Neutral/03 rounded py-1.5 px-4">
-                <div className="text-base text-Neutral/04 mb-4">
-                  Permissions
+          <form onSubmit={()=> console.log("salom")}>
+            <div className="flex mb-7">
+              <div className="grow pr-7 flex justify-between flex-wrap">
+                <div className="w-full">
+                  <Input
+                    handleChange={handleChange}
+                    touched={touched}
+                    values={values}
+                    errors={errors}
+                    type={"full_name"}
+                    inputType={"text"}
+                    placeholderText={"Full name"}
+                  />
                 </div>
-                <div className="flex flex-wrap text-Neutral/Shades/04-75%">
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check1"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
-                    />
-                    <label for="check1">Permission 1</label>
-                  </span>
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check2"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
-                    />
-                    <label for="check2">Permission 1</label>
-                  </span>
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check3"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
-                    />
-                    <label for="check3">Permission 1</label>
-                  </span>
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check4"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
-                    />
-                    <label for="check4">Permission 1</label>
-                  </span>
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check5"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded border"
-                    />
-                    <label for="check5">Permission 1</label>
-                  </span>
-                  <span className="items-center flex w-1/3 mb-3">
-                    <input
-                      id="check6"
-                      type="checkbox"
-                      // onChange={checkedAll}
-                      // checked={true}
-                      className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
-                    />
-                    <label for="check6">Permission 1</label>
-                  </span>
+                <div className="w-[calc((100%-20px)/2)] text-Neutral/Shades/04-75% text-[15px]">
+                  <ReactSelect
+                    handleChange={handleChange}
+                    touched={touched}
+                    values={values}
+                    errors={errors}
+                    type={"role"}
+                    position={position}
+                  />
                 </div>
-              </div>
-            </div>
-            <div className="flex-none w-[400px] pt-3 px-[18px] pb-5 rounded border border-Neutral/03 flex flex-col">
-              <span className="block mb-2.5 text-sm text-Neutral/04 leading-5 flex-none">
-                Admin photo
-              </span>
-              <div className="border border-dashed border-Neutral/Shade/04-40% rounded-lg w-full grow">
-                {true ? (
-                  <div className="flex flex-col items-center w-full h-full justify-center">
-                    <span>{img_svg}</span>
-                    <span className="text-xs text-Neutral/Shades/04-75% mt-4">
-                      Image size 400x400 px is recommended
+                <div className="w-[calc((100%-20px)/2)]">
+                  <Input
+                    handleChange={handleChange}
+                    touched={touched}
+                    values={values}
+                    errors={errors}
+                    type={"email"}
+                    inputType={"email"}
+                    placeholderText={"Email"}
+                  />
+                </div>
+                <div className="w-[calc((100%-20px)/2)]">
+                  <Input
+                    handleChange={handleChange}
+                    touched={touched}
+                    values={values}
+                    errors={errors}
+                    type={"phone"}
+                    inputType={"text"}
+                    placeholderText={"Phone number"}
+                  />
+                </div>
+                <div className="w-[calc((100%-20px)/2)]">
+                  <Input
+                    handleChange={handleChange}
+                    touched={touched}
+                    values={values}
+                    errors={errors}
+                    type={"password"}
+                    inputType={"password"}
+                    placeholderText={"Password"}
+                  />
+                </div>
+                <div className="w-full border border-Neutral/03 rounded py-1.5 px-4">
+                  <div className="text-base text-Neutral/04 mb-4">
+                    Permissions
+                  </div>
+                  <div className="flex flex-wrap text-Neutral/Shades/04-75%">
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check1"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
+                      />
+                      <label htmlFor="check1">Permission 1</label>
+                    </span>
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check2"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
+                      />
+                      <label htmlFor="check2">Permission 1</label>
+                    </span>
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check3"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
+                      />
+                      <label htmlFor="check3">Permission 1</label>
+                    </span>
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check4"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
+                      />
+                      <label htmlFor="check4">Permission 1</label>
+                    </span>
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check5"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded border"
+                      />
+                      <label htmlFor="check5">Permission 1</label>
+                    </span>
+                    <span className="items-center flex w-1/3 mb-3">
+                      <input
+                        id="check6"
+                        type="checkbox"
+                        // onChange={checkedAll}
+                        // checked={true}
+                        className="w-[20px] h-[20px] mr-2.5 cursor-pointer rounded"
+                      />
+                      <label htmlFor="check6">Permission 1</label>
                     </span>
                   </div>
-                ) : (
-                  <div></div>
-                )}
+                </div>
+              </div>
+              <div className="flex-none w-[400px] pt-3 px-[18px] pb-5 rounded border border-Neutral/03 flex flex-col">
+                <span className="block mb-2.5 text-sm text-Neutral/04 leading-5 flex-none">
+                  Admin photo
+                </span>
+                <div className="border relative border-dashed border-Neutral/Shade/04-40% rounded-lg w-full grow">
+                  {true ? (
+                    <div className="flex flex-col items-center w-full h-full justify-center">
+                      <span>{img_svg}</span>
+                      <span className="text-xs text-Neutral/Shades/04-75% mt-4">
+                        Image size 400x400 px is recommended
+                      </span>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                  <input
+                    type="file"
+                    name="resume"
+                    // onChange={(e) => {
+                    //   if (e.target.files[0]) {
+                    //     selected(e.target.files[0]);
+                    //   }
+                    // }}
+                    className="absolute file:hidden text-transparent w-full h-full top-0 left-0 rounded cursor-pointer after:hidden before:hidden"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex justify-end text-sm leading-6">
-            <span className="p-2 text-center rounded border border-Neutral/Shade/04-40% min-w-[130px] text-Neutral/Shades/04-75% hover:bg-Neutral/02 cursor-pointer mr-5">Cancel</span>
-            <span className="p-2 text-center rounded border border-Neutral/Shade/04-40% min-w-[130px] text-white bg-Primary/03 cursor-pointer hover:bg-hoverButton">Save and Add</span>
-          </div>
+            <div className="flex justify-end text-sm leading-6">
+              <span className="p-2 text-center rounded border border-Neutral/Shade/04-40% min-w-[130px] text-Neutral/Shades/04-75% hover:bg-Neutral/02 cursor-pointer mr-5">
+                Cancel
+              </span>
+              <span className="p-2 text-center rounded min-w-[130px] text-white bg-Primary/03 cursor-pointer hover:bg-hoverButton">
+                Save and Add
+              </span>
+            </div>
+          </form>
         </div>
       </div>
     </div>

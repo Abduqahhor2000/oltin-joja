0bac817d0c6c704681c224c86f82dd310bb1d397
unknown <muhriddinxusniddinov57@gmail.com>
2022-12-13 13:42:13 +0500

    feature/add-product login spinner

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -9,7 +9,7 @@ function createInstance(baseURL){
             // Authorization:`Bearar ${JSON.parse(
             //     localStorage.getItem("Authorization")
             // )}`,
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZnVsbF9uYW1lIjoiU2lyb2ppZGluIElzbW9pbG92IiwicGhvbmUiOiIrOTk4OTMwNTQwOTA1IiwiYmlydGhfZGF0ZSI6IjIwMDItMDktMDVUMDA6MDA6MDAuMDAwWiIsImF2YXRhciI6ImF2YXRhciIsInJvbGUiOiJzdXBlcl9hZG1pbiIsImNoYXRfaWQiOm51bGwsImNyZWF0ZWRfYXQiOiIyMDIyLTEyLTEyVDA3OjUzOjUxLjEzMFoiLCJ1c2VyX2lkIjoxLCJpYXQiOjE2NzA4MzMwMDgsImV4cCI6MTY3MDg2OTAwOH0.dMTxHJ6P8-e2rROr5Z-Ta7aWRKfQyfwJVCF_su4tJzE`
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZnVsbF9uYW1lIjoiU2lyb2ppZGluIElzbW9pbG92IiwicGhvbmUiOiIrOTk4OTMwNTQwOTA1IiwiYmlydGhfZGF0ZSI6IjIwMDItMDktMDVUMDA6MDA6MDAuMDAwWiIsImF2YXRhciI6ImF2YXRhciIsInJvbGUiOiJzdXBlcl9hZG1pbiIsImNoYXRfaWQiOm51bGwsImNyZWF0ZWRfYXQiOiIyMDIyLTEyLTEzVDA1OjU0OjMwLjIzMVoiLCJ1c2VyX2lkIjoxLCJpYXQiOjE2NzA5MTY5MjAsImV4cCI6MTY3MDk1MjkyMH0._lwQVGtKmPoO088gpjo1j95WMWgs2hjwEZ8P2-gUx5M`
          }  
     })
 }

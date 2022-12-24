f4aa19f2321005d24b8715678bb7d0816736dac1
unknown <muhriddinxusniddinov57@gmail.com>
2022-12-24 10:42:39 +0500

    feature/add product invoice detail and invoice list page is completed

--- src/pages/Invoice/InvoiceList.js
+++ src/pages/Invoice/InvoiceList.js
@@ -1,126 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { del_grey_svg, del_svg } from "../../svg/product";
 import { search_svg } from "../../svg/navbar";
+import { notification } from "../../toastify/Toastify";
 import { send_svg, send_grey_svg } from "../../svg/product";
+import { usePost } from "../../api/http";
 
 function InvoiceList() {
-  const [invoices, setInvoices] = useState([
-    {
-      id: 12155,
-      orderName: "French Fries",
-      orderId: 1,
-      invoiceDate: "14:04 19/01/2021",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12156,
-      orderName: "Mango Pie",
-      orderId: 2,
-      invoiceDate: "14:04 20/01/2021",
-      quantity: 14,
-      price: "1.110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12157,
-      orderName: "FrieD Egg Sandwich",
-      orderId: 3,
-      invoiceDate: "14:04 21/01/2021",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12158,
-      orderName: "Lemon Yogurt Parfait",
-      orderId: 4,
-      invoiceDate: "14:04 22/01/2021",
-      quantity: 14,
-      price: "25.110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12159,
-      orderName: "Spicy Grill Sandwich",
-      orderId: 5,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12160,
-      orderName: "Chicken Sandwich",
-      orderId: 6,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12161,
-      orderName: "Veg Sandwich",
-      orderId: 7,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12162,
-      orderName: "Cake",
-      orderId: 8,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12163,
-      orderName: "Chicken Fried",
-      orderId: 9,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12164,
-      orderName: "Nachos",
-      orderId: 10,
-      invoiceDate: "14:04 02/02/2022",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12165,
-      orderName: "Spaghetti",
-      orderId: 11,
-      invoiceDate: "14:04 19/01/2021",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-    {
-      id: 12166,
-      orderName: "Pastries",
-      orderId: 12,
-      invoiceDate: "14:04 19/01/2021",
-      quantity: 14,
-      price: "110.000",
-      totalBill: "140.000",
-    },
-  ]);
+  const [state, setState] = useState({
+    deliveryDateFrom: "2019-08-24 14:15:22.123",
+  });
+  const [data, setData] = useState([]);
+  const FetchData = () => {
+    usePost("/v1/orders/invoice-details", state)
+      .then((res) => setData(res?.data?.ordersByOrganizations[0]?.orders))
+      .catch((err) => console.log(err));
+  };
+  console.log(data);
 
+  useEffect(() => {
+    FetchData();
+  }, []);
   return (
     <div>
       <div className="py-5 px-10">
-        <div >
-          
-        </div>
+        <div></div>
         <div className="bg-white rounded-[10px] overflow-hidden">
           <div className="pt-5 pl-7 flex justify-between">
             <span className="text-lg leading-6 text-Neutral/04">
@@ -136,19 +39,13 @@ function InvoiceList() {
                 {search_svg}
               </span>
             </span>
-            
           </div>
           <div className="overflow-auto relative max-h-[calc(100vh-180px)]">
             <table className="w-full text-sm text-Neutral/Shades/04-75%">
               <thead>
                 <tr className="text-Neutral/Shade/07-50% sticky top-0 bg-white z-10">
                   <th>
-                    <div className="border-b-2 border-Neutral/03 text-start py-4 pl-7">
-                      Invoice Id
-                    </div>
-                  </th>
-                  <th>
-                    <div className="text-start border-b-2 border-Neutral/03 font-semibold py-4">
+                    <div className="text-start border-b-2 border-Neutral/03 font-semibold py-4 pl-7">
                       Order Name
                     </div>
                   </th>
@@ -173,59 +70,42 @@ function InvoiceList() {
                     </div>
                   </th>
                   <th>
-                    <div className="text-center border-b-2 border-Neutral/03 font-semibold py-4">
-                      Total Bill
+                    <div className="text-start border-b-2 border-Neutral/03 font-semibold py-4">
+                      Total Price
                     </div>
                   </th>
-                  {/* <th>
-                    <div className="text-center border-b-2 border-Neutral/03 font-semibold py-4">
-                      Send Invoice
-                    </div>
-                  </th> */}
                 </tr>
               </thead>
               <tbody>
-                {invoices.map((invoice, index, array) => {
+                {data.map((invoice, index, array) => {
+                  var quantity = "";
+                  var order_name = "";
+                  var price = "";
+                  var cost = "";
+                  invoice?.order?.items?.forEach((element) => {
+                    order_name = element?.product?.name;
+                    quantity = element?.amount;
+                    price = element?.price;
+                    cost = element?.cost;
+                  });
+                  const Invoice_date = invoice.order?.whenClosed;
+                  const spliceData = Invoice_date.slice(0, 16);
+
                   return (
                     <tr
                       key={invoice.id}
                       className={`border-b border-Neutral/03`}
                     >
-                      <td className="py-3 pl-7 min-w-[135px]">{invoice.id}</td>
-                      <td className="py-1.5 min-w-[230px]">
-                        {invoice.orderName}
+                      <td className="py-1.5 min-w-[150px]   pl-7">
+                        {order_name}
                       </td>
-                      <td className="py-3 min-w-[100px]">{invoice.orderId}</td>
-                      <td className="py-2 min-w-[160px]">
-                        {invoice.invoiceDate}
+                      <td className="py-3 min-w-[100px]">
+                        {invoice?.order?.number}
                       </td>
-                      <td className="py-[14px] min-w-[90px]">
-                        {invoice.quantity}
-                      </td>
-                      <td className="py-3 min-w-[130px]">{invoice.price}</td>
-                      <td className="py-3 min-w-[130px] text-center">
-                        {invoice.totalBill}
-                      </td>
-                      {/* <td className="min-w-[135px] leading-5">
-                        <div className="flex w-full justify-center">
-                          <span className="p-1 group cursor-pointer w-8 h-8 select-none pt-1.5 mr-1.5">
-                            <span className="hidden group-hover:block">
-                              {send_svg}
-                            </span>
-                            <span className="block group-hover:hidden">
-                              {send_grey_svg}
-                            </span>
-                          </span>
-                          <span className="p-1 cursor-pointer group select-none ml-1.5">
-                            <span className="hidden group-hover:block">
-                              {del_svg}
-                            </span>
-                            <span className="block group-hover:hidden">
-                              {del_grey_svg}
-                            </span>
-                          </span>
-                        </div>
-                      </td> */}
+                      <td className="py-2 min-w-[160px]">{spliceData}</td>
+                      <td className="py-[14px] min-w-[90px] pl-2">{quantity}</td>
+                      <td className="py-3 min-w-[130px]">{price}</td>
+                      <td className="py-3 min-w-[130px] ">{cost}</td>
                     </tr>
                   );
                 })}
